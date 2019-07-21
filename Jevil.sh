bfcce9d54461f34b08a4badbe4ad4d75ed4d68ff
Jevil36239 <51107256+Jevil36239@users.noreply.github.com>
2019-07-21 17:18:35 +0700

    Create Jevil.sh

---
+++ Jevil.sh
@@ -0,0 +1,399 @@
+#usr/bin/bash
+clear
+#ini warna√
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+# Recode ajh mmnx
+echo
+echo
+#ini paket untuk menjalankan tools
+Jevil.sh
+echo
+clear
+echo
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo $i  "AUTHOR   : Jevil AlChaos"
+sleep 1
+echo $i  "Team.    :Attacker Alone Team"
+sleep 1
+echo $i  "Sekutu.  :Black Panther Team"
+sleep 1
+echo $i  "TOOLS    : KAGAK JELAS MAKLUMKAN YANG BUAT JEVIL"
+sleep 1
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+GNU nano 4.3                    JevilTools.sh
+echo $cy "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+echo
+echo
+echo "###DAFTAR TOOLS###"
+echo
+echo $cy "[1]"$bi" Brute Force Facebook"
+echo "=================================================="
+echo $cy "[2]"$i" Banguni Sahur"
+echo "=================================================="
+echo $cy "[3]"$me" Skull Project"
+echo "=================================================="
+echo $cy "[4]"$ku" Termux Alice (Khusus Jones)"
+echo "=================================================="
+echo $cy "[5]"$pur" Mata mata"
+echo "=================================================="
+echo $cy "[6]"$pu" Trap Project"
+echo "=================================================="
+echo $cy "[7]"$cy" Sadap Camera"
+echo "=================================================="
+echo $cy "[8]"$bi" Hammer DDOS"
+echo "=================================================="
+echo $cy "[9]"$me "Peneration Red Hawk"
+echo "=================================================="
+echo $cy "[10]"$ku" Lacak Ip"
+echo "=================================================="
+echo $cy "[11]"$pur" Tools Hack Fb Lengkap"
+echo "=================================================="
+echo $cy "[12]"$pu" SQLMap"
+echo "=================================================="
+echo $cy "[13]"$cy" Hydra Penetration Test"
+echo "=================================================="
+echo $cy "[14]"$bi" Buat Deface"
+echo "=================================================="
+echo $cy "[15]"$me" Profile Guard Facebook"
+echo "=================================================="
+echo $cy "[16]"$ku" Facebook Report Auto Ban"
+echo "=================================================="
+echo $cy "[17]"$pur" Kumpulan Virtex"
+echo "=================================================="
+echo $cy "[18]"$pu" Hack Wifi ( ROOT )"
+echo "=================================================="
+echo $cy "[19]"$cy" Spam Call"
+echo "=================================================="
+echo $cy "[20]"$bi"  CCTV Jevil ( Masih 30% ) "
+echo  "================================================="
+echo $cy "[21]"$me" Ddos Jevil  ( Beta )"
+echo "=================================================="
+echo $cy "[22]"$ku"  Facebook Phising ( Shellphish )"
+echo "=================================================="
+echo $cy "[23]"$pur" Facebook Phising ( BlackEye )"
+echo "=================================================="
+echo $cy "[24]"$pu" Facebook Phising ( Social Fish )"
+echo "=================================================="
+echo $cy "[25]"$cy" Mempercantik Termux"
+echo "=================================================="
+echo $cy "[26]"$bi" Web Vuln Scanner"
+echo "=================================================="
+echo $cy "[27]"$me"  Deface Website"
+U nano 4.3                    JevilTools.sh
+echo "=================================================="
+echo $cy "[28]"$ku" Deface Tebas Index ( Ada Passwordnya )"
+echo "=================================================="
+echo $cy "[29]"$pur" Tools Weeman"
+echo "=================================================="
+echo $cy "[30]"$pu" Gado Gado ( Bukan Makanan :v )"
+echo "=================================================="
+echo $cy "[66]"$me" EXIT"
+echo "=================================================="
+echo
+echo $cy"┌==="$bi"[ CHAOS ]"
+echo $cy"¦"
+read -p">>>" pil;
+
+if [ $pil = 1 ]
+then
+clear
+pkg install php -y
+pkg install git -y
+pkg install python2 -y
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+bash new.sh
+fi
+
+if [ $pil = 2 ]
+then
+clear
+git clone https://github.com/zlucifer/troll_project
+cd troll_project
+bash troll.sh
+fi
+
+if [ $pil = 3 ]
+then
+clear
+git clone https://github.com/zlucifer/skull_project
+cd  skull_project
+bash skull.sh
+fi
+
+if [ $pil = 4 ]
+then
+clear
+git clone https://github.com/zlucifer/termux_alice
+cd termux_alice
+sh alice.sh
+fi
+
+if [ $pil = 5 ]
+then
+clear
+git clone https://github.com/zlucifer/paladin_project
+cd paladin_project
+sh paladin.sh
+fi
+
+if [ $pil = 6 ]
+then
+clear
+git clone https://github.com/zlucifer/trap_project
+cd trap_project
+bash trap.sh
+fi
+
+if [ $pil = 7 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 8 ]
+then
+clear
+git clone https://github.com/cyweb/hammer
+cd hammer
+python3 hammer.py
+fi
+
+if [ $pil = 9 ]
+then
+clear
+git clone https://github.com/Tuhinshubhra/RED_HAWK
+cd RED_HAWK
+php rhawk.php
+fi
+
+if [ $pil = 10 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+[ $pil = 10 ]
+then
+clear
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 11 ]
+then
+clear
+git clone https://github.com/FR13ND8/BRUTEFORCEnew
+cd BRUTEFORCEnew
+sh new.sh
+fi
+
+if [ $pil = 12 ]
+then
+clear
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python sqlmap.py
+fi
+
+if [ $pil = 13 ]
+then
+clear
+git clone https://github.com/ory/hydra
+cd hydra
+./install.sh
+fi
+
+if [ $pil = 14 ]
+then
+clear
+git clone https://github.com/404rgr/webdav77
+chmod +x *
+./install.sh
+bash webdav77.sh
+fi
+
+if [ $pil = 15 ]
+then
+git clone https://github.com/FR13ND8/ProfileGuardFb
+cd ProfileGuardFb
+php Guard.php
+fi
+
+if [ $pil = 16 ]
+then
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 16 ]
+then
+git clone https://github.com/IlayTamvan/Report
+cd Report
+unzip Report.zip
+python2 Report.py
+fi
+
+if [ $pil = 17 ]
+then
+git clone  https://github.com/muhammadfathul/VIRTEX
+cd VIRTEX
+sh Virtex.sh
+fi
+
+if [ $pil = 18 ]
+then
+git clone https://github.com/esc0rtd3w/wifi-hacker
+cd wifi-hacker
+chmod +x *
+sh wifi-hacker.sh
+fi
+
+if [ $pil = 19 ]
+then
+git clone https://github.com/underxploit/enoxuia
+cd enoxuia
+php enox.php
+fi
+
+if [ $pil = 20 ]
+then
+git clone https://github.com/Jevil36239/JevilCCTV
+cd JevilCCTV
+sh tvjev.sh
+fi
+
+if [ $pil = 21 ]
+then
+apt upgrade
+apt update
+pkg install git
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/Jevil36239/JevilDdos
+cd JevilDdos
+python2 JevilDdos.py
+fi
+
+if [ $pil = 21 ]
+then
+apt upgrade
+apt update
+pkg install git
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/Jevil36239/JevilDdos
+cd JevilDdos
+python2 JevilDdos.py
+fi
+
+if [ $pil = 22 ]
+then
+git clone https://github.com/thelinuxchoice/shellphish
+cd shellphish
+bash shellphish sh
+fi
+
+
+if [ $pil = 23 ]
+then
+git clone https://github.com/thelinuxchoice/blackeye
+cd blackeye
+bash blackeye.sh
+fi
+
+
+if [ $pil = 24 ]
+then
+git clone https://github.com/UndeadSec/SocialFish
+cd SocialFish
+chmod +x *
+python2 SocialFish.py
+fi
+
+
+if [ $pil = 25 ]
+then
+git clone https://github.com/kadal15/LazyGo-v1
+cd Lazygo-v1
+sh lazygo.sh
+fi
+
+
+if [ $pil = 26 ]
+then
+git clone https://github.com/E4rr0r4/XGDork
+cd XGDork
+python2 XGDork.py
+fi
+
+if [ $pil = 27 ]
+then
+git clone https://github.com/Amriez/AOCDEFACE
+cd AOCDEFACE
+chmod +x *
+clear
+sh AOC.sh
+fi
+
+if [ $pil = 28 ]
+then
+echo $cy"Sc Ini Ada Password nya"
+sleep 2
+echo $cy"Cepat Salin Password Di Bawah Ini"
+sleep 2
+echo $cy"Kamu Akan Di Beri 5 Detik"
+sleep 2
+echo $cy"Password : 8UJWI9-OIKSNJ-0OKSMA"
+sleep 5
+clear
+git clone https://github.com/R133F/1ND3X
+cd 1ND3X
+sh tebas.sh
+fi
+
+if [ $pil = 29 ]
+then
+git clone https://github.com/evait-security/weeman
+cd weeman
+chmod +x *
+python2 weeman.py
+fi
+
+if [ $pil = 30 ]
+then
+pkg install php
+pkg install gem
+git clone https://github.com/Senitopeng/GadoGado
+cd GadoGado
+bash gado
+fi
+
+if [ $pil = 66 ]
+then
+clear
+figlet -f slant "E X I T"|lolcat
+sleep 2
+echo $cy"Terima Kasih Sudah Mampir Kesini"
+sleep 2
+echo $i"Bila Ada Bug Jangan Tanyakan Ke Saya :v"
+sleep 2
+echo $i"Team :"$i" Attacker Alone Team"
+echo $bi"Sekutu :"$i" Black Panther Team"
+exit
+fi
